fc9b9524439e2904a28175475038029421fc7f6d
Ananda <ananda852003@gmail.com>
2019-06-27 17:44:31 +0100

    Update WristLunateAnalysis.m

--- WristLunateAnalysis.m
+++ WristLunateAnalysis.m
@@ -19,6 +19,16 @@ infoDicom = dicominfo('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT6\ST
 % readDicom = dicomread('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT8\STD1\SER1\IMG0');  % LA -good
 % infoDicom = dicominfo('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT8\STD1\SER1\IMG0');
 
+% readDicom = dicomread('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT11\STD1\SER1\IMG0');  % LA -good
+% infoDicom = dicominfo('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT11\STD1\SER1\IMG0');
+
+% readDicom = dicomread('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT12\STD1\SER2\IMG0'); %LA-good
+% infoDicom = dicominfo('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT12\STD1\SER2\IMG0');
+
+% readDicom = dicomread('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT10\STD1\SER2\IMG0'); %PA-good aligned,bad crop
+% infoDicom = dicominfo('C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT10\STD1\SER2\IMG0');
+
+
 readDicomD=double(readDicom);
 
 BW= edge(readDicom,'canny');
@@ -29,29 +39,33 @@ thetaPeak = theta(P(1,2));
 dicomRotate2 = imrotate(readDicom,thetaPeak);
                     
                     
-dicomRotRemoveEdge2=removeDicomEdges(dicomRotate2);
+%dicomRotRemoveEdge2=removeDicomEdges(dicomRotate2); %original
+dicomRotRemoveEdge2=RemoveDicomEdges2(dicomRotate2); %modifications
 
             % pixel-value cross-sections along line segments
                      AxisX = [0 size(dicomRotRemoveEdge2,2)];
                      AxisY = [size(dicomRotRemoveEdge2,1)/2 size(dicomRotRemoveEdge2,1)/2];
                      AxisY1 = [size(dicomRotRemoveEdge2,1)/3 size(dicomRotRemoveEdge2,1)/3];
-                     profileDicom =improfile(dicomRotRemoveEdge2,AxisX,AxisY1);
+                     
+                     profileDicom =improfile(dicomRotRemoveEdge2,AxisX,AxisY);
+                    %profileDicom2=improfile(dicomRotRemoveEdge2,AxisX,AxisY,700);
                      
 %                      [minvalue, index_of_min] = min(profileDicom);
 %                      [maxvalue, index_of_max] = max(profileDicom);                     
 
                      edgeDicom = edge(dicomRotRemoveEdge2,'Canny');
                      
-                     dicomRotRemoveEdge3 = RemoveDicomEdges2(dicomRotRemoveEdge2);
+                     dicomRotRemoveEdge3 = RemoveDicomEdges2(dicomRotRemoveEdge2); %test
                      
                     
                      %crop image from the hand and above
                      cropDicomImage = imcrop(dicomRotRemoveEdge2,[AxisX,AxisY]);
+                     
                      cropDicomImageD=double(cropDicomImage);
                      
                      %[rows,cols]        = size(readDicom);
-                     [rows,cols] =size(cropDicomImage);
-                     [rows3,cols3]        = size(dicomRotate2);
+                     [rows,cols]         = size(cropDicomImage);
+                     [rows3,cols3]       = size(dicomRotate2);
                      
 %                      cumIntProj_Rot_Col          = sum(readDicom)/rows;  
 %                      cumIntProj_Rot_Row          = sum(readDicom,2)/cols;
@@ -169,43 +183,41 @@ dicomRotRemoveEdge2=removeDicomEdges(dicomRotate2);
                 %edgeremoval 
                 marginRemove = 30;
                 %newDicom = dicomRotate2;
-                newDicom = cropDicomImage;
-                %LeftColumn
-                    initCol = max(1,round( locs_Col(1)-w_Col(1)-marginRemove ));
-                    finCol  = min(cols3,round(marginRemove+locs_Col(1)+w_Col(1)  ));
-                    newDicom(:,initCol:finCol) = ColsLeft;
-
-                 %RightColumn
-
-                    initCol = max(1,round( locs_Col(end)-w_Col(end)-marginRemove ));
-                    finCol  = min(cols3,round(marginRemove+locs_Col(end)+w_Col(end)  ));
-                    newDicom(:,initCol:finCol) = ColsRight;
-
-% %                  %RowAbove
+%                 newDicom = cropDicomImage;
+%                 %LeftColumn
+%                     initCol = max(1,round( locs_Col(1)-w_Col(1)-marginRemove ));
+%                     finCol  = min(cols3,round(marginRemove+locs_Col(1)+w_Col(1)  ));
+%                     newDicom(:,initCol:finCol) = ColsLeft;
+% 
+%                  %RightColumn
+% 
+%                     initCol = max(1,round( locs_Col(end)-w_Col(end)-marginRemove ));
+%                     finCol  = min(cols3,round(marginRemove+locs_Col(end)+w_Col(end)  ));
+%                     newDicom(:,initCol:finCol) = ColsRight;
+% 
+% % %                  %RowAbove
 %                     initRow = max(1,round( locs_Row(1)-w_Row(1)-marginRemove));
 %                     finRow = min(rows3,round(marginRemove+locs_Row(1)+w_Row(1)  ));
 %                     newDicom(initRow:finRow,:) = RowsAbove;
-% 
-%                  %RowBelow
+% % 
+% %                  %RowBelow
 %                     initRow = max(1,round( locs_Row(end)-w_Row(end)-marginRemove));
 %                     finRow = min(rows3,round(marginRemove+locs_Row(end)+w_Row(end) ) );
 %                     newDicom(initRow:finRow,:) = RowsBelow;
 
                 % Return to initial position
                 %Xray5                   = imrotate(Xray4,-angleRot);
-                [rows5,cols5] = size(newDicom);
-                rows6 = floor((rows5-rows)/2);
-                cols6 = floor((cols5-cols)/2);
-                Xray6 = newDicom(rows6+1:end,cols6+1:end);
-                Xray6(rows+1:end,:)=[];
-                Xray6(:,cols+1:end)=[];                    
+%                 [rows5,cols5] = size(newDicom);
+%                 rows6 = floor((rows5-rows)/2);
+%                 cols6 = floor((cols5-cols)/2);
+%                 Xray6 = newDicom(rows6+1:end,cols6+1:end);
+%                 Xray6(rows+1:end,:)=[];
+%                 Xray6(:,cols+1:end)=[];                    
                      
-                     
-                  
                      
 figure, 
 subplot(1,2,1), imagesc(readDicom),colormap bone, title('original dicom');
-subplot(1,2,2), plot(mean(readDicom,2)), title('mean values of original dicom ');
+subplot(1,2,2), plot(mean(readDicom,2)), title('mean values of original dicom '); %mean of row
 
 figure,
 subplot(1,2,1), imagesc(dicomRotRemoveEdge2),colormap bone, title('edge aligned and edge removed');
@@ -214,15 +226,21 @@ subplot(1,2,2), plot(mean(dicomRotRemoveEdge2,2)), title('mean values of edge al
 figure,
 subplot(1,2,2), plot(profileDicom),title('pixel-value cross-sections');
 subplot(1,2,1), imagesc(dicomRotRemoveEdge2),colormap bone,hold on , plot(AxisX,AxisY,'r'),title('mixed')
+%subplot(1,3,3), plot(profileDicom2),title('pixel-value cross-sections v2');
 %subplot(1,5,5),imshow(edgeDicom), title('Edge of Cropped Image');
 
  figure,
- imagesc(cropDicomImage),colormap bone, title('location based cropped');
+ imagesc(cropDicomImage),colormap bone, title('auto location based cropped');
  %subplot(1,2,1),%subplot(1,2,2), ,title('image data peaks')
+ %set(gcf,'position',[321         381        1000         400]);
+%  t= im2bw(readDicom);
  
- figure,
- imagesc(newDicom),colormap bone, title('boundary');
+ %  figure,
+ %imagesc(newDicom),colormap bone, title('boundary');
 
+%  currentFile = 'C:\Users\aczf102\Documents\MATLAB\Data\Normals\N1\PAT6\STD1\SER1\IMG0';
+%  cropDicomImageMask= zeros(size(cropDicomImage));
+%  [AreaInflammation,widthAtCM,inflammationLines,inflamationLimits]    = analyseLandmarkLunate_Exp (cropDicomImage,cropDicomImageMask,infoDicom,currentFile,1);
 
 
 
